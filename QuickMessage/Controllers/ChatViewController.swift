92fca1a67e42e8119ac05f94466629817bf67407
Enrique Gongora <EGTech24@gmail.com>
2020-10-29 16:20:19 -0700

    Created the ChatView controller. Added the necessary textfields, buttons and tableview to display messages

---
+++ QuickMessage/Controllers/ChatViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatViewController.swift
+//  QuickMessage
+//
+//  Created by Enrique Gongora on 10/29/20.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
