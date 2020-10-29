efc40457806d73d6f90a374eb38a972afc9da892
Enrique Gongora <EGTech24@gmail.com>
2020-10-29 14:49:45 -0700

    Created the initial view controller. Added register and login button and label. Added constraints with no errors

--- QuickMessage/WelcomeViewController.swift
+++ QuickMessage/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  QuickMessage
 //
 //  Created by Enrique Gongora on 10/29/20.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
