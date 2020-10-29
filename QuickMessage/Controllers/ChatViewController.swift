6cee81ae274670a26829a0372dba2ab48bacc6cd
Enrique Gongora <EGTech24@gmail.com>
2020-10-29 16:36:05 -0700

    Embded our initial VC in a Navigation Controller. Created Segues to properly go from VC to VC

--- QuickMessage/Controllers/ChatViewController.swift
+++ QuickMessage/Controllers/ChatViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
