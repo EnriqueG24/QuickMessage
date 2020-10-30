757532bc777c9ff4479d02e858021f8cf08d9e46
Enrique Gongora <EGTech24@gmail.com>
2020-10-29 17:00:49 -0700

    Created a typewriter like animation for the initial view controller

--- QuickMessage/Controllers/WelcomeViewController.swift
+++ QuickMessage/Controllers/WelcomeViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️QuickMessage"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
+    
 }
