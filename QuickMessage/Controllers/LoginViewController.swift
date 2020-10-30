3f62716b63c2be874eebdb01389f787076a8b6f6
Enrique Gongora <EGTech24@gmail.com>
2020-10-30 16:39:44 -0700

    users can successfully login with their credentials. Fixed constraints on labels.

--- QuickMessage/Controllers/LoginViewController.swift
+++ QuickMessage/Controllers/LoginViewController.swift
@@ -6,9 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
+    }
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
