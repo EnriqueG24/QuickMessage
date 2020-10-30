d767e7ea46cb59fca77108b3748bac9876a1949d
Enrique Gongora <EGTech24@gmail.com>
2020-10-30 15:55:28 -0700

    Users can successfully register with Firebase/Auth. Password textfields are now secure. Created the proper IBOutlets/Actions

--- QuickMessage/Controllers/RegisterViewController.swift
+++ QuickMessage/Controllers/RegisterViewController.swift
@@ -6,9 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if error != nil {
+                    // TODO - Create a notification to show the user the error
+                    print(error?.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+    }
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
