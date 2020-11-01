a3a74c7b45fcca83e345969a3e5a0613f871312f
Enrique Gongora <EGTech24@gmail.com>
2020-10-31 18:31:32 -0700

    Created a custom designed message cell. The corner are rounded and the cell grows as the message gets longer. Removed the tableViewCell we created before in favor of the custom designed one.

---
+++ QuickMessage/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  QuickMessage
+//
+//  Created by Enrique Gongora on 10/31/20.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // By tapping into the UIView frame.size.height so the corner radius adapts to the height as the view gets taller. 
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
